48b9cd66866d9f5a6f30580c8164a7db49c810e4
mktcode <kontakt@markus-kottlaender.de>
2019-12-07 09:20:31 +0100

    let level up animation emit light

--- src/scenes/gui.js
+++ src/scenes/gui.js
@@ -214,6 +214,13 @@ export default class GuiScene extends Phaser.Scene {
       this.registry.set('skillPoints', this.registry.get('skillPoints') + 1)
       const currentDungeon = this.scene.get('Dungeon' + this.registry.get('currentDungeon'))
       currentDungeon.hero.sprites.levelUp.anims.play('levelUp')
+      currentDungeon.lightManager.lights.push({
+        sprite: currentDungeon.hero.sprites.levelUp,
+        intensity: () => 1
+      })
+      currentDungeon.time.delayedCall(1000, () => {
+        currentDungeon.lightManager.removeLight(currentDungeon.hero.sprites.levelUp)
+      })
     }
     const lastMaxXp = Hero.getXpForLevelUp(xpBasedLevel)
     const currentMaxXp = Hero.getXpForLevelUp(xpBasedLevel + 1)
