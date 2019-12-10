057233bdb0ecd64e294abbd98bfcd0cb01e6452d
mktcode <kontakt@markus-kottlaender.de>
2019-12-10 04:34:37 +0100

    adjusted skill menu positions and added close button

--- src/scenes/character.js
+++ src/scenes/character.js
@@ -18,14 +18,25 @@ export default class CharacterScene extends Phaser.Scene {
     const centerY = this.game.scale.height / 2
 
     const availableSkillPoints = this.add
-      .text(centerX, centerY - 80, 'Skill Points: ' + (this.registry.get('skillPoints') - this.registry.get('skillPointsSpent')), {
+      .text(centerX - 125, centerY - 80, 'Skill Points: ' + (this.registry.get('skillPoints') - this.registry.get('skillPointsSpent')), {
         font: "24px monospace",
         fill: "#ffffff",
         padding: { x: 20, y: 10 }
       })
 
+    const close = this.add
+      .text(centerX - 50, centerY - 44, 'close', {
+        font: "20px monospace",
+        fill: "#ffffff",
+        padding: { x: 20, y: 10 }
+      })
+      .setInteractive();
+    close.on('pointerup', () => {
+      this.scene.sleep()
+    })
+
     const moreHealth = this.add
-      .text(centerX, centerY, 'Health: ' + this.registry.get('maxHealth'), {
+      .text(centerX - 85, centerY, 'Health: ' + this.registry.get('maxHealth'), {
         font: "24px monospace",
         fill: "#000000",
         padding: { x: 20, y: 10 },
@@ -41,7 +52,7 @@ export default class CharacterScene extends Phaser.Scene {
     })
 
     const moreDamage = this.add
-      .text(centerX, centerY + 50, 'Damage: ' + this.registry.get('damage'), {
+      .text(centerX - 85, centerY + 55, 'Damage: ' + this.registry.get('damage'), {
         font: "24px monospace",
         fill: "#000000",
         padding: { x: 20, y: 10 },
@@ -56,7 +67,7 @@ export default class CharacterScene extends Phaser.Scene {
     })
 
     const moreTorchDuration = this.add
-      .text(centerX, centerY + 100, 'Torch Duration: ' + this.registry.get('torchDuration') + 's', {
+      .text(centerX - 157, centerY + 110, 'Torch Duration: ' + this.registry.get('torchDuration') + 's', {
         font: "24px monospace",
         fill: "#000000",
         padding: { x: 20, y: 10 },
